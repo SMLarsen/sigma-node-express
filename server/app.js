d12f8ca7922be061142e69a1fc399041d2483458
Steve Larsen <stevelarsen01@gmail.com>
2016-11-08 16:14:24 -0600

    add date added to song and module with date getter

--- server/app.js
+++ server/app.js
@@ -1,8 +1,29 @@
+// Instructions
+// Take the lecture code and add some logic to our POST route on the server. Do this logic on the server!
+//
+// - Don't allow the user to add duplicate songs.
+// - Don't allow the user to add songs with a blank artist or title field.
+// - Before pushing to our array, add a property to the new song object for the
+//    dateAdded with the current date. You'll have to look up the Date object in
+//    javascript.
+//
+// - Add the dateAdded to our DOM display for our songs.
+//
+// - NOTE: You're going to have to send back a status code of 400 if there are
+ // problems. That means your client code needs to be able to handle the error
+ // case and tell the user!
+//
+// HARD MODE
+// - Move your new logic into a module (like in Module Madness) and export the
+//  functions needed. This keeps the code nice and organized. Have a look at the
+//  Router() object in Express.
+
 // node/express application
 var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
+var date = require('./modules/utils/getDate');
 
 // puts post request body data and store it on req.body
 app.use(bodyParser.urlencoded({extended: true}));
@@ -13,10 +34,12 @@ app.set('port', process.env.PORT || 3000);
 var songs = [
   {
     artist: "Bruce Springsteen",
-    title: "Born in the U.S.A."
+    title: "Born in the U.S.A.",
+    dateAdded: date.today
   }
 ];
 
+console.log(songs);
 // Routes
 app.post('/songs', function(req, res) {
   // req.body is supplied by bodyParser above
