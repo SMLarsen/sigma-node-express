7be05ef9da615b50a449bc3467c5efcc36090c15
Steve Larsen <stevelarsen01@gmail.com>
2016-11-08 18:25:40 -0600

    add comments and beautify

--- server/app.js
+++ server/app.js
@@ -1,22 +1,18 @@
-// Instructions
-// Take the lecture code and add some logic to our POST route on the server. Do this logic on the server!
-//
 // - DONE: Don't allow the user to add duplicate songs.
 // - DONE: Don't allow the user to add songs with a blank artist or title field.
 // - DONE: Before pushing to our array, add a property to the new song object for
 //    the dateAdded with the current date.
 // - DONE: Add the dateAdded to our DOM display for our songs.
-//
 // - DONE: NOTE: You're going to have to send back a status code of 400 if
 //  there are problems. That means your client code needs to be able to
 //  handle the error case and tell the user!
-//
-// HARD MODE
 // - DONE: Move your new logic into a module (like in Module Madness) and export
 //    the functions needed. This keeps the code nice and organized. Have a look
- //   at the Router() object in Express.
+//   at the Router() object in Express.
+
 
 // node/express application
+
 var express = require('express');
 var app = express();
 var path = require('path');
@@ -26,45 +22,49 @@ var valDupSong = require('./modules/valDupSong');
 var valEmptyFields = require('./modules/valEmptyFields');
 
 // puts post request body data and store it on req.body
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.set('port', process.env.PORT || 3000);
 
-// Our song data
-var songs = [
-  {
+// Build initial song data
+var songs = [{
     artist: "Bruce Springsteen",
     title: "Born in the U.S.A.",
     dateAdded: date.today
-  }
-];
+}];
 
-// Routes
+// ===================  Routes  ===========================
+
+// add new song from from client to song array
 app.post('/songs', function(req, res) {
-  // req.body is supplied by bodyParser above
-  var newSong = req.body;
-  if (valDupSong(songs, newSong) || valEmptyFields(newSong)) {
-    res.sendStatus(400);
-  } else {
-    newSong.dateAdded = date.today;
-    songs.push(newSong);
-    res.sendStatus(201);
-  }
+    // req.body is supplied by bodyParser above
+    var newSong = req.body;
+    if (valDupSong(songs, newSong) || valEmptyFields(newSong)) {
+        res.sendStatus(400);
+    } else {
+        newSong.dateAdded = date.today;
+        songs.push(newSong);
+        res.sendStatus(201);
+    }
 });
 
+// get and send songs to client
 app.get('/songs', function(req, res) {
-  res.send(songs);
+    res.send(songs);
 });
 
 // static file routing
 app.get('/*', function(req, res) {
-  var file = req.params[0] || '/views/index.html';
-  console.log(file);
+    var file = req.params[0] || '/views/index.html';
+    console.log(file);
 
-  res.sendFile(path.join(__dirname, './public/', file));
-  // /public/views/index.html
+    res.sendFile(path.join(__dirname, './public/', file));
+    // /public/views/index.html
 });
 
+// initiation of server
 app.listen(app.get('port'), function() {
-  console.log('Server is listening on port ' + app.get('port'));
+    console.log('Server is listening on port ' + app.get('port'));
 });
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,67 +1,70 @@
 $(document).ready(function() {
 
-  getSongs();
+    getSongs(); // get songs to load on initial display
 
-  // =============  listeners  =================================
-  $("#postSongForm").on("submit", function(event) {
-    event.preventDefault();
-    var newSong = {};
+    // =============  listeners  =================================
+    $("#postSongForm").on("submit", function(event) {
+        event.preventDefault();
+        var newSong = {};
 
-    $.each($('#postSongForm').serializeArray(), function(i, field) {
-      newSong[field.name] = field.value;
+        $.each($('#postSongForm').serializeArray(), function(i, field) {
+            newSong[field.name] = field.value;
+        });
+
+        addNewSong(newSong);
     });
 
-    addNewSong(newSong);
-  });
+    // get songs from server
+    function getSongs() {
+        $.ajax({
+            type: 'GET',
+            url: '/songs',
+            success: function(songData) {
+                songsToDom(songData);
+            }
+        });
+    }
 
-
-  function getSongs() {
-    $.ajax({
-      type: 'GET',
-      url: '/songs',
-      success: function(songData) {
-        songsToDom(songData);
-      }
-    });
-  }
-
-      function addNewSong(newSong) {
+    // validate and add new song to server array
+    function addNewSong(newSong) {
         if (validateInput(newSong)) {
             // send song object to the Server
-          $.ajax({
-            type: 'POST',
-            url: '/songs',
-            data: newSong,
-            success: function(response) {
-              console.log(response);
-              getSongs();
-            },
-            error: function(error) {
-              alert("Oh no! Your song didn't save correctly.");
-            }
-          });
+            $.ajax({
+                type: 'POST',
+                url: '/songs',
+                data: newSong,
+                success: function(response) {
+                    console.log(response);
+                    getSongs();
+                },
+                error: function(error) {
+                    alert("Oh no! Your song didn't save correctly.");
+                }
+            });
         }
-      }
-
-  function songsToDom(songs) {
-    $("#songContainer").empty();
-
-    for (var i = 0; i < songs.length; i++) {
-      $("#songContainer").append('<div class="song"></div>');
-      var $el = $("#songContainer").children().last();
-      $el.append('<h3>' + songs[i].title + '</h3>');
-      $el.append('<p>By: ' + songs[i].artist + '</p>');
-      $el.append('<p>Date Added: ' + songs[i].dateAdded + '</p>');
     }
-  }
 
-  function validateInput(newSong) {
-    if (newSong.artist === "" || newSong.title === "") {
-      alert("Artist and Title must be entered.");
-      return false;
-    } else {
-      return true;
+    // load songs from server on the DOM
+    function songsToDom(songs) {
+        $("#songContainer").empty();
+
+        for (var i = 0; i < songs.length; i++) {
+            $("#songContainer").append('<div class="song"></div>');
+            var $el = $("#songContainer").children().last();
+            $el.append('<h3>' + songs[i].title + '</h3>');
+            $el.append('<p>By: ' + songs[i].artist + '</p>');
+            $el.append('<p>Date Added: ' + songs[i].dateAdded + '</p>');
+        }
     }
-  }
+
+    // client-side validation for empty input fields
+    function validateInput(newSong) {
+        if (newSong.artist === "" || newSong.title === "") {
+            alert("Artist and Title must be entered.");
+            return false;
+        } else {
+            return true;
+        }
+    }
 
 });
